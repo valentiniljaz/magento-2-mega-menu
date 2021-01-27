c64f583f91f1d1b67fe072dfb950961f1c6c0a63
Valentin Iljaž <valentin@progmbh.com>
2021-01-27 11:56:24 +0100

    Enable installation through symlink and fix icons for mega menu

--- registration.php
+++ registration.php
@@ -3,5 +3,5 @@
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
     'Magiccart_Magicmenu',
-    __DIR__
+    isset($file) ? dirname($file) : __DIR__
 );
